499f3894c79b6726c9dd93939159c1831350cdb3
Aleeza <alizey649@gmail.com>
2025-04-25 02:26:14 +0500

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,6 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.MODE === "development"
     ? "http://localhost:5001/api"
-    : "https://chitchat-by-aleeza.railway.app/api", // ✅ Add your full Railway backend URL here
+    : "https://chitchat-by-aleeza.railway.app/", // ✅ Add your full Railway backend URL here
   withCredentials: true,
 });
